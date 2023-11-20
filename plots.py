9b70493574bb878660917600c6afb3c9aa96ee8a
Santiago Ballerini <sballerini@itba.edu.ar>
2023-11-19 21:46:33 -0300

    feat: add zoom to plot

--- plots.py
+++ plots.py
@@ -7,17 +7,28 @@ def plot_errors_per_epoch(errors_per_epoch: List[float]):
     plt.xlabel("Epochs")
     plt.ylabel("Error")
 
-    min_error = min(errors_per_epoch)
-    max_error = max(errors_per_epoch)
-    step = int((max_error - min_error) / 10)
-
-    ticks = [i for i in range(int(min_error) + step, int(max_error) - step, step)]
-    ticks.append(max_error)
-    ticks.insert(0, min_error)
-    print(ticks)
-    plt.yticks(ticks)
+    plt.yticks([i for i in range(0, 100, 10)])
 
     x_ticks = [i for i in range(0, len(errors_per_epoch), 10000)]
-    x_ticks.append(len(errors_per_epoch))
+
+    # Zoom in
+    plt.ylim(0, 100)
 
     plt.savefig("errors_per_epoch.png")
+
+    plt.figure()
+
+    plt.plot(errors_per_epoch)
+    plt.xlabel("Epochs")
+    plt.ylabel("Error")
+
+    plt.yticks([i for i in range(0, 10)])
+    plt.xticks(x_ticks)
+
+    # Line at 1
+    plt.axhline(y=1, color="r", linestyle="-")
+
+    # Zoom in
+    plt.ylim(0, 10)
+
+    plt.savefig("errors_per_epoch_zoom.png")
