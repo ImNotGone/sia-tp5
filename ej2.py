9f2d03e3988669fa1960342cb96332241e309a94
Franco Bosetti <francobosetti@live.com.ar>
2023-11-21 00:51:15 -0300

    chore: vae 2??????
    Co-Authored-By: Gonzalo Martone <70904263+ImNotGone@users.noreply.github.com>

--- ej2.py
+++ ej2.py
@@ -6,6 +6,8 @@ from dataset_loaders import (load_emoji_data, load_font_data)
 from vae_activations import (relu, sigmoid, tanh, identity, selu)
 from autoencoder import VAE
 
+from vaev2 import (VariationalAutoencoder, AdamOptimization, SigmoidActivationFunction)
+
 from optimization_methods import get_optimization_method
 
 from utils import (
@@ -23,7 +25,7 @@ import pickle, gzip
 
 
 def main():
-    data = load_font_data()
+    data = load_emoji_data()
 
     # Flatten the matrix into a vector
     data = data.reshape((data.shape[0], -1))
@@ -43,32 +45,32 @@ def main():
         errors_per_epoch = []
         learning_rate = 0.1
         loss_func = squared_error
-        act_func = sigmoid
+        optimization_method = AdamOptimization
+        act_func = SigmoidActivationFunction
 
         # 64 -> 16 -> 2 -> 16 -> 64
-        vae = VAE(
-            [[35, 16], [16, 35]],
+        vae = VariationalAutoencoder(
+            64,
             2,
-            learning_rate,
             max_epochs,
-            batch_size,
-            loss_func,
-            act_func
+            [64, 16],
+            [16, 64],
+            act_func,
+            optimization_method
         )
 
-        vae.learn(data)
+        vae.train(data)
 
         reconstructed_fonts = []
         original_fonts = []
         for emoji in data:
             reconstructed_sample = vae.encode_decode(emoji[None,:])
-            reconstructed_font = reconstructed_sample.reshape((7, 5))
+            reconstructed_font = reconstructed_sample.reshape((8, 8))
             reconstructed_fonts.append(reconstructed_font)
-            original_fonts.append(emoji.reshape((7, 5)))
+            original_fonts.append(emoji.reshape((8, 8)))
 
-        create_image(original_fonts, "original.png", (7, 5))
-        create_image(reconstructed_fonts, "reconstructed.png", (7, 5))
-
+        create_image(original_fonts, "original.png", (8, 8))
+        create_image(reconstructed_fonts, "reconstructed.png", (8, 8))
 
         # plot_errors_per_epoch(errors_per_epoch)
 
